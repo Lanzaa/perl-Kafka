cbb6fd1d2dc2646a1c2ec00c52fcac8c2c304ca1
Colin Blower <cblower@barracuda.com>
2013-05-07 16:38:33 -0700

    Add debug flag to topic lister.

--- examples/topic_lister.pl
+++ examples/topic_lister.pl
@@ -1,4 +1,4 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -I ../lib -I lib
 
 use strict;
 use warnings;
@@ -7,28 +7,37 @@ use warnings;
 # This is an example Perl Kafka program that can list the topics and number of partitions for a broker group.
 #
 my $USAGE = <<USAGE;
-> ./topic_lister.pl --broker localhost:9092
+Example usage:
+> ./topic_lister.pl --broker localhost:9092 [--broker localhost:9093 ...] [--debug]
+
+USAGE
+
+my $EXAMPLE_OUTPUT = <<OUTPUT;
+> ./topic_lister.pl --broker localhost:9092 [--broker localhost:9093 ...] [--debug]
 Topics found:
 * 'testTopic'
 * 'BaconBacon'
 * 'water-unicorns'
 
-USAGE
+OUTPUT
 
 # For getting topic metadata we only need a connection to the brokers
-use Kafka::BrokerChannel; 
+use Kafka::BrokerChannel;
 
 use Getopt::Long;
 use Data::Dumper;
 
 my @brokers = ();
+my $debug = 0;
 
 GetOptions(
     "broker=s" => \@brokers, # Get broker list from the command line
+    "debug", \$debug,
 );
 
 if (scalar(@brokers) == 0) {
-    @brokers = ("localhost:9092"); # default broker
+    print $USAGE;
+    exit(1);
 }
 my $broker_list = join(",", @brokers);
 my $channel = Kafka::BrokerChannel->new(
@@ -44,5 +53,21 @@ my @topics = $channel->topic_list();
 print "Topics found:\n";
 foreach my $topic (@topics) {
     print "* '$topic'\n";
+    if ($debug) {
+        my $topic_metadata = $channel->getTopicMetadata($topic);
+        my @partitions = keys $topic_metadata;
+        foreach my $partition (sort @partitions) {
+            my $data = $topic_metadata->{$partition};
+            #while (my ($partition, $data) = each($topic_metadata)) {
+            printf("\tpartition: %d\tleader: %d\tisr: %s\treplicas: %s\terror: %d\n",
+                $partition,
+                $data->{leader},
+                "[".join(", ", @{$data->{isr}})."]",
+                "[".join(", ", @{$data->{replicas}})."]",
+                $data->{error_code}
+            );
+        }
+
+    }
 }
 
